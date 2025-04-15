0c9981174d7d99901f56ac7110d255ea3c99a49c
PalakTyagi123 <tyagipalak710@gmail.com>
2025-04-15 14:33:41 +0530

    recipe

---
+++ day3.js
@@ -0,0 +1,36 @@
+// function testFunction(){
+//     console.log();
+// }
+// testFunction();
+
+// function testFunction(x)
+// {
+//     console.log(x);
+// }
+// testFunction("palak");
+// function testFunction(x,y)
+// {
+//     return x+y;
+// }
+// let c=testFunction(3,5);
+// console.log(c);
+// XYZ();
+// XYZ(10);
+// XYZ(10,"palak");
+// let t=function XYZ()
+// {
+//     console.log("This is function expression")
+
+// }
+// t();
+// let t=function()
+// {
+//     console.log("this is an annoymous function")
+// }
+// t();
+// Arrow function
+// let t=()=>{
+//     console.log("Arrow function");
+
+// }
+// t();
